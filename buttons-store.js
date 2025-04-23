cbc4b4147a0d23470bc0ed70144c2e55933e768e
BenQuack <bquacken@uvm.edu>
2025-04-22 21:41:43 -0400

    simple cart implementation

--- buttons-store.js
+++ buttons-store.js
@@ -1,9 +1,41 @@
-for (let i = 1; i <= 100; i++) {
+let cart = []
+
+for (let i = 1; i <= 9; i++) {
     const button = document.getElementById(`b${i}`);
     if (button) {
-      button.addEventListener("click", () => {
-        console.log(`Button ${i} clicked`);
-        // do something with i
-      });
+      button.addEventListener("click", () => addToCart(`"b${i}"`));
+      console.log(i)
     }
+}
+
+for (let i = 1; i <= 6; i++){
+  const button = document.getElementById(`f${i}`)
+  if (button){
+    button.addEventListener('click', () => addToCart(`"f${i}"`));
+    console.log(i)
+  }
+}
+
+class Item {
+  constructor(name, amount){
+    this.name = name;
+    this.amount = amount;
+  }
+
+  addAnother(){
+    this.amount += 1
+  }
+
+}
+
+function addToCart(id){
+  for (let i=0; i < cart.length; i++){
+    if (cart[i].name == id){
+      cart[i].add_another()
+      return
+    }
+  }
+  cart.push(new Item(id,1))
+  console.log(cart)
+  return
 }
