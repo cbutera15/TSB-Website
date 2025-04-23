48ec6bf75ad61bb9bb2eaf983c31c58c2fe413cc
BenQuack <bquacken@uvm.edu>
2025-04-23 11:28:10 -0400

    cart/checkout demo almost done

--- buttons-store.js
+++ buttons-store.js
@@ -1,4 +1,7 @@
+const cartDisplay = document.getElementById("cart-display")
+const priceDisplay = document.getElementById("total-price")
 let cart = []
+
 
 for (let i = 1; i <= 9; i++) {
     const button = document.getElementById(`b${i}`);
@@ -20,6 +23,7 @@ class Item {
   constructor(name, amount){
     this.name = name;
     this.amount = amount;
+    this.price = 10;
   }
 
   addAnother(){
@@ -31,11 +35,32 @@ class Item {
 function addToCart(id){
   for (let i=0; i < cart.length; i++){
     if (cart[i].name == id){
-      cart[i].add_another()
+      cart[i].addAnother()
+      updateCart()
       return
     }
   }
   cart.push(new Item(id,1))
   console.log(cart)
+  updateCart()
   return
+}
+
+function updateCart() {
+  cartDisplay.textContent = "";
+  priceDisplay.textContent = "TOTAL: "
+  if (cart.length === 0) {
+    cartDisplay.innerHTML = "No items in cart.";
+    priceDisplay.textContent += "0"
+    return;
+  }
+  let sum = 0
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i];
+    let li = document.createElement("li")
+    li.textContent = `${item.name} x ${item.amount} - $${item.amount * item.price}`
+    cartDisplay.appendChild(li);
+    sum += item.amount * item.price
+  }
+  priceDisplay.textContent += `${sum}`
 }
