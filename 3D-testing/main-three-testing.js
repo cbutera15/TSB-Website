c49da896194fc8a8a9e24293f28410d31694411c
Colin Butera <ccmjbutera@gmail.com>
2025-06-05 12:24:32 -0400

    added load bar to all pages

--- 3D-testing/main-three-testing.js
+++ 3D-testing/main-three-testing.js
@@ -5,7 +5,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
